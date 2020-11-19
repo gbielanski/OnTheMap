b5f0841ccebe43d75244166c976629bbf71eb1f6
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2020-11-19 09:12:10 +0100

    Add LoginTextField

---
+++ OnTheMap/View/LoginTextField.swift
@@ -0,0 +1,34 @@
+//
+//  LoginTextView.swift
+//  OnTheMap
+//
+//  Created by Grzegorz Bielanski on 19/11/2020.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class LoginTextField: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 5
+        tintColor = UIColor.white
+        backgroundColor = UIColor(named: "PrimaryLight")
+
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        let insetBounds = CGRect(x: bounds.origin.x + 8, y: bounds.origin.y, width: bounds.size.width - 16, height: bounds.size.height)
+        return insetBounds
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        let insetBounds = CGRect(x: bounds.origin.x + 8, y: bounds.origin.y, width: bounds.size.width - 16, height: bounds.size.height)
+        return insetBounds
+    }
+    
+}
