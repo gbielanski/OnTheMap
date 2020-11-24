a0ec3fb56878b4125b5aec7af4b7186d9e53278e
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2020-11-24 18:36:36 +0100

    Add taskForGETRequest

--- OnTheMap/Model/Client/UdacityClient.swift
+++ OnTheMap/Model/Client/UdacityClient.swift
@@ -90,4 +90,37 @@ class UdacityClient{
     
     task.resume()
   }
+
+  @discardableResult class func taskForGETRequest<ResponseType: Decodable>(url: URL, responseType: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void) -> URLSessionTask{
+      let task = URLSession.shared.dataTask(with: url) { data, response, error in
+          guard let data = data else {
+              DispatchQueue.main.async {
+                  completion(nil, error)
+              }
+              return
+          }
+          let decoder = JSONDecoder()
+          do {
+              let responseObject = try decoder.decode(ResponseType.self, from: data)
+              DispatchQueue.main.async {
+                  completion(responseObject, nil)
+              }
+          } catch {
+              do {
+                  print("errorResponse")
+                  let errorResponse = try decoder.decode(UdacityErrorResponse.self, from: data)
+                  DispatchQueue.main.async {
+                      completion(nil, errorResponse)
+                  }
+              }catch{
+                  DispatchQueue.main.async {
+                      completion(nil, error)
+                  }
+              }
+          }
+      }
+      task.resume()
+      return task
+
+  }
 }
