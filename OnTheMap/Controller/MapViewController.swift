a668b2200e7ae85dce5509cfd70332c864e4a735
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2020-11-26 15:23:19 +0100

    Add Pins to map

--- OnTheMap/Controller/MapViewController.swift
+++ OnTheMap/Controller/MapViewController.swift
@@ -7,8 +7,40 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+  @IBOutlet weak var mapView: MKMapView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    _ = UdacityClient.getStudentLocations { students, error in
+      StudentModel.studentlist = students
+      self.updateMap()
+    }
+
+  }
+
+  private func updateMap(){
+    var annotations = [MKPointAnnotation]()
+
+    for student in StudentModel.studentlist {
+
+      let lat = CLLocationDegrees(student.latitude)
+      let long = CLLocationDegrees(student.longitude)
+
+      let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+
+      let annotation = MKPointAnnotation()
+      annotation.coordinate = coordinate
+      annotation.title = "\(student.firstName.notEmptyString()) \(student.lastName.notEmptyString())"
+      annotation.subtitle = student.mediaURL.withUrlSchema()
+
+      annotations.append(annotation)
+    }
+
+    self.mapView.addAnnotations(annotations)
+  }
 
 }
 
