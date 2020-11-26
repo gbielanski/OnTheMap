14603d4683aeba35e7e8e1b6fdd1788f5ba1747e
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2020-11-27 00:52:04 +0100

    Add sign up link

--- OnTheMap/Controller/LoginViewController.swift
+++ OnTheMap/Controller/LoginViewController.swift
@@ -8,16 +8,27 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextViewDelegate {
   
   @IBOutlet weak var emailTextField: UITextField!
   @IBOutlet weak var passwordTextField: UITextField!
   @IBOutlet weak var loginButton: UIButton!
   @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-  
+  @IBOutlet weak var textView: UITextView!
+
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    let attributedString = NSMutableAttributedString(string: "Don't have an account. Sign up")
+    attributedString.addAttribute(.link, value: "https://auth.udacity.com/sign-up", range: NSRange(location: 23, length: 7))
+    let style = NSMutableParagraphStyle()
+    style.alignment = NSTextAlignment.center
+    attributedString.addAttributes([.paragraphStyle : style], range: NSRange(location: 0, length: 30))
+
+    attributedString.addAttributes([.font : UIFont.systemFont(ofSize: 15)], range: NSRange(location: 0, length: 30))
+
+    textView.attributedText = attributedString
+    textView.delegate = self
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -58,6 +69,12 @@ class LoginViewController: UIViewController {
     let alertVC = UIAlertController(title: "Login Failed", message: message, preferredStyle: .alert)
     alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
     show(alertVC, sender: nil)
+  }
+
+  func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+    print("textView")
+      UIApplication.shared.open(URL)
+      return false
   }
   
 }
