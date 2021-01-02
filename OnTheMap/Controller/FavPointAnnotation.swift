cc0818ca23ea0284c13246b4a3ec37ca75068dd8
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2021-01-02 17:04:36 +0100

    Center map on the first found favorite restaurant

---
+++ OnTheMap/Controller/FavPointAnnotation.swift
@@ -0,0 +1,13 @@
+//
+//  FavPointAnnotation.swift
+//  OnTheMap
+//
+//  Created by Grzegorz Bielanski on 31/12/2020.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import Foundation
+
+class FavPointAnnotation: MKPointAnnotation {
+  var 
+}
